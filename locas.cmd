b68d8db1f536ba302cf6c441ecad502b178f7d9c
Linos <153276567+Linos1391@users.noreply.github.com>
2024-10-29 23:53:10 -0400

    Update locas.cmd

--- locas.cmd
+++ locas.cmd
@@ -4,7 +4,7 @@
 fi
 #UNIX
 
-# -- Add Conda or Docker here. Eg: conda activate ... ; python ./LocalAssistant/parser.py $@
+# -- Add Conda or Docker here. Eg: conda activate ... ; python "$LocalAssistant/LocalAssistant/parser.py" $@
 python "$LocalAssistant/LocalAssistant/parser.py" $@
 
 exit 0
@@ -13,5 +13,5 @@ exit 0
 
 :WINDOWS
 
-REM -- Add Conda or Docker here. Eg: conda activate ... && python LocalAssistant/parser.py %*
+REM -- Add Conda or Docker here. Eg: conda activate ... && python "%LocalAssistant%/LocalAssistant/parser.py" %*
 python "%LocalAssistant%/LocalAssistant/parser.py" %*

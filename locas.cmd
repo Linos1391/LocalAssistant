214b69ed295cd004a5f1467822b1002208cf68c0
Linos <153276567+Linos1391@users.noreply.github.com>
2024-10-29 20:45:48 -0400

    Update and rename locas.sh to locas.cmd

---
+++ locas.cmd
@@ -0,0 +1,17 @@
+:; if [ -z 0 ]; then
+  @echo off
+  goto :WINDOWS
+fi
+#UNIX
+
+# -- Add Conda or Docker here. Eg: conda activate ... | python ./LocalAssistant/parser.py $@
+python ./LocalAssistant/parser.py $@
+
+exit 0
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+:WINDOWS
+
+REM -- Add Conda or Docker here. Eg: conda activate ... && python LocalAssistant/parser.py %*
+conda activate LocalAssistant && python LocalAssistant/parser.py %*
--- locas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ./LocalAssistant/parser.py $@

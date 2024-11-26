6ef8d9041fb17d5366de3c75177a79f9c51faeae
Linos <153276567+Linos1391@users.noreply.github.com>
2024-11-25 23:52:32 -0500

    Update utils.py

--- LocalAssistant/utils.py
+++ LocalAssistant/utils.py
@@ -2,11 +2,14 @@ import pathlib
 import shutil
 import json
 import os
+import logging
 
 # Path
 PROJECT_PATH: str = pathlib.Path(__file__).parent
 MODEL_PATH: str = PROJECT_PATH / 'models'
 USER_PATH: str = PROJECT_PATH / 'users'
+
+LOGGER = logging.getLogger()
 
 # Custom Exception
 class LocalAssistantException(Exception):
@@ -15,26 +18,25 @@ class LocalAssistantException(Exception):
     """
     pass
 
-def _clean_cache(path: str, verbose: bool) -> None:
+def _clean_cache(path: str) -> None:
     """
     delete choosen .cache dir (Too dangerous to be used)
     """
     try:
         shutil.rmtree(path)
-        if verbose:
-            print(f'Cleaned {path}.')
+        LOGGER.debug(f'Cleaned {path}.')
     except: # it doesn't matter
-        if verbose:
-            print(f'Cannot find {path}, skipped.')
+        LOGGER.debug(f'Cannot find {path}, skipped.')
 
-def clean_all_cache(verbose: bool) -> None:
+def clean_all_cache() -> None:
     """
     delete all .cache dir
     """
     for path in (
         MODEL_PATH / '.cache',
     ):
-        _clean_cache(path, verbose)
+        _clean_cache(path)
+        LOGGER.info('Cleared all cache')
         
 # it happens that i'm too smart.
 def _print_dict(data: dict, level: int=0):
@@ -42,7 +44,7 @@ def _print_dict(data: dict, level: int=0):
         if isinstance(value, str):
             print(level*'   ' + f"'{key}': '{value}',")
         else: # value is dict
-            print(level*'   ' + "%r: {" % (key))
+            print(level*'   ' + "%r: {" % (key)) # use %r as '{' is written
             _print_dict(value, level+1)
             print(level*'   ' + "},")
         
@@ -50,40 +52,36 @@ class LocalAssistantConfig:
     PATH: str = PROJECT_PATH / 'locas_config.json'
     DATA: dict = {}
         
-    def upload_config_file(self, verbose: bool) -> None:
+    def upload_config_file(self) -> None:
         # dump data to file.
         with open(self.PATH, mode="w", encoding="utf-8") as write_file:
             json.dump(self.DATA, write_file, indent=4)
             write_file.close()
 
-        if verbose:
-            print("Uploaded current data to config.json file.")
+        LOGGER.info("Uploaded current data to config.json file.")
 
-    def get_config_file(self, verbose: bool) -> None:
+    def get_config_file(self) -> None:
         try:
-            if verbose:
-                print('Finding locas_config.json file.')
+            LOGGER.debug('Finding locas_config.json file.')
 
             # Read the data
             with open(self.PATH, mode="r", encoding="utf-8") as read_file:
                 self.DATA = json.load(read_file)
                 read_file.close()
 
-            if verbose:
-                print('Found locas_config.json file.')
+            LOGGER.debug('Found locas_config.json file.')
         except:
-            if verbose:
-                print('Cannot find locas_config.json file. Create new one.')
+            LOGGER.debug('Cannot find locas_config.json file. Create new one.')
 
             self.DATA = {
                 "hf_token": "", # Hugging Face token.
                 "load_in_bits": "8", # 'quantization' method. (So the device won't blow up)
                 "models": { # the model that being use for chatting.
-                    "Text_Generation": "Qwen",
-                    "Tokenizer": "Qwen",
+                    "Text_Generation": "",
                 },
                 "users": {
-                    "current": "", # the current user that being used.
+                    "current": "1", # the current user that being used.
+                    "1": "default",
                 }
 
             }
@@ -92,6 +90,7 @@ class LocalAssistantConfig:
             with open(self.PATH, mode="w", encoding="utf-8") as write_file:
                 json.dump(self.DATA, write_file, indent=4)
                 write_file.close()
+        LOGGER.info('Got data from config file.')
 
     def print_config_data(self) -> None:
         _print_dict(self.DATA)
@@ -112,10 +111,9 @@ class LocalAssistantConfig:
                 return (True, str(index))
         return (False, '0')
 
-    def remove_user_with_index(self, verbose: bool, target_index: str) -> None:
+    def remove_user_with_index(self, target_index: str) -> None:
         # move up until last user
-        if verbose:
-            print(f'Delete user {self.DATA['users'][target_index]}')
+        LOGGER.info(f'Delete user {self.DATA['users'][target_index]}')
         for index in range(int(target_index), len(self.DATA['users'])):
             try:
                 self.DATA['users'].update({str(index): self.DATA['users'][str(index+1)]})
@@ -125,4 +123,5 @@ class LocalAssistantConfig:
         if len(self.DATA['users']) == 2:
             self.DATA['users'].update({"current": ""})
                 
-        self.upload_config_file(verbose)
+        self.upload_config_file()
+

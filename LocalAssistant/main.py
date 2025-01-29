79188e1a4dd3647355cc8a1c790a64b341010952
linos1391 <linos.coding@gmail.com>
2025-01-28 20:03:57 -0500

    Fix showing models in config

--- LocalAssistant/main.py
+++ LocalAssistant/main.py
@@ -131,14 +131,18 @@ Choose from: '4', '8', 'None'.", _check_valid):
                     while True:
                         print('Choose from:')
 
-                        model: str = config.data['models'][command]
                         folder_model: list = []
-                        for item in os.scandir(os.path.join(utils_ext.model_path, model)):
+                        for item in os.scandir(os.path.join(utils_ext.model_path, command)):
                             if item.is_dir():
                                 folder_model.append(item.name)
                                 print(f'    - {item.name}')
 
-                        if not _change_single(model, ''):
+                        def _check_valid(command: str) -> bool:
+                            if command in folder_model:
+                                return True
+                            return False
+
+                        if not _change_single(command, '', _check_valid):
                             break
                         continue
 
@@ -180,7 +184,7 @@ process stop when similiarity score is lower.", _check_valid):
 
             case 'users':
                 print("Type 'locas user -h' for better configuration.\n")
-                continue
+                break
 
 def _user(parser_arg: argparse.Namespace):
     """User command function."""

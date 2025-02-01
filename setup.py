29d3330b6aedc2ce9edc0a95714fac3e07aa9cff
linos1391 <linos.coding@gmail.com>
2025-02-01 18:15:29 -0500

    Automatic for `setup.py`

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 """Setup file."""
 
 import pathlib
+import os
 import setuptools
+
+PARENT: pathlib.Path = pathlib.Path(__file__).parent
+
+def _read_requirements():
+    requirement_path: str = PARENT / 'requirements.txt'
+    packages = open(requirement_path, encoding='utf-8').readlines()
+    return [package.removesuffix('\n') for package in packages]
+
+def _read_version():
+    init_path: str = os.path.join(PARENT, 'LocalAssistant', '__init__.py')
+    return open(init_path, encoding='utf-8').read().split()[-1][1:-1]
 
 setuptools.setup(
     name="LocalAssistant",
-    version="1.1.1",
+    version=_read_version(),
     description="LocalAssistant (locas) is an AI designed to be used in CLI. \
 (Currently in development)",
     long_description=pathlib.Path('README.md').read_text(encoding="utf-8"),
@@ -28,14 +40,7 @@ setuptools.setup(
         "Operating System :: Unix",
     ],
     python_requires=">=3.10",
-    install_requires=[
-        "transformers[torch]",
-        "bitsandbytes",
-        "sentence-transformers",
-        "PyMuPDF",
-        "sentencepiece",
-        "pyvis",
-    ],
+    install_requires=_read_requirements(),
     packages=setuptools.find_packages(),
     include_package_data=True,
     entry_points={"console_scripts": ["locas = LocalAssistant.main:main"]},
